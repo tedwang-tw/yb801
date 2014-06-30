da8e33253a57e77eeb7650aa0ebebc9f37724d39
Ted Wang <tedwang.tw@gmail.com>
2014-06-30 22:29:55 +0800

    Change configuration file to ETL global config file.

--- etl/tfidf_detail.js
+++ etl/tfidf_detail.js
@@ -30,7 +30,7 @@ var basename_err = 'error.txt';
 var basename_status = 'status.txt';
 var filename_status;
 
-var filename_preset = path.join(__dirname, 'tfidf_detail_cfg.json');
+var filename_preset = path.join(__dirname, 'etl_config.json');
 var presetList;
 
 var keywords = [];
