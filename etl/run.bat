d3f6d2f94fd832e51d41321b70b7eb493c4b8991
Ted Wang <tedwang.tw@gmail.com>
2014-07-06 00:08:09 +0800

    Add batch file for re-generating TFIDF from new corpus.

---
+++ etl/run.bat
@@ -0,0 +1,11 @@
+node corpus_detail.js
+pause
+
+node synonym_detail.js
+pause
+
+node corpus_merge.js
+pause
+
+copy corpus_merge\104\job\20140524\2007001000\keywords_merge.txt input\.
+node tfidf_detail.js
