5a3762d8579861c183690093d854c57fbaef0e29
Ted Wang <tedwang.tw@gmail.com>
2014-07-27 17:53:47 +0800

    1. Remove old results before each step. 2. Copy results to the formal output folder.

--- run_resume.sh
+++ run_resume.sh
@@ -1,17 +1,26 @@
 #!/bin/sh
 cd crawler
 
+rm -r -f raw/104/vip/20140716/*.html
+rm -r -f text/104/vip/20140716/*.txt
+rm -r -f ../etl/text/104/job/20140716/resume/*.txt
+
 node vip_members.js demo
 
 echo -n "Press any key... "
 read text
 
+
 cd ../etl
+
+rm -r -f corpus/104/job/20140716/resume/*.json
 
 node corpus_detail.js resume
 
 echo -n "Press any key... "
 read text
+
+rm -r -f synonym/104/job/20140716/resume/*.json
 
 node synonym_detail.js resume
 
@@ -24,21 +33,30 @@ echo -n "Press any key... "
 read text
 
 #cp corpus_merge/104/job/20140716/2007001000/keywords_merge.txt input/.
+rm -r -f tfidf/104/job/20140716/resume/*.txt
 
 node tfidf_detail.js resume
 
-echo -n "Press any key... "
+echo -n "Press any key or CTRL-C... "
 read text
 
 cp tfidf/104/job/20140716/resume/resumelist.txt input/.
 
 node similarity_job.js
 
-echo -n "Press any key... "
+echo -n "Press any key or CTRL-C... "
 read text
 
+rm -r -f recommend/104/job/20140716/2007001000/MA_KM/*.json
 cp similarity/104/job/20140716/resume/sim_resume.txt input/.
 
 node recommend_job.js
+
+echo -n "Press any key or CTRL-C... "
+read text
+
+rm -r -f ../output/recommendation/*.json
+cp recommend/104/job/20140716/2007001000/MA_KM/*.json ../output/recommendation
+
 
 cd ..
