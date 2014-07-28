3a9eed9d092e0af819e3d05f156ecf458903e208
Ted Wang <tedwang.tw@gmail.com>
2014-07-29 00:20:04 +0800

    Add socket.io for remote control.

---
+++ ws_demo_server.js
@@ -0,0 +1,167 @@
+var httpd = require('http').createServer(handler);
+var io = require('socket.io').listen(httpd);
+var fs = require('fs');
+var child_process = require('child_process');
+var count = 0;
+var sno = 0;
+
+httpd.listen(4000);
+function handler(req, res) {
+	fs.readFile(__dirname + '/index_demo.html',
+		function (err, data) {
+		if (err) {
+			res.writeHead(500);
+			return res.end('Error loading index.html');
+		}
+		res.writeHead(200);
+		res.end(data);
+	});
+}
+
+function run_cmd(broadcast, cmd) {
+	var cmds = cmd.split(/\s/);
+	var op1 = cmds.length > 1 ? cmds[1] : '';
+	var child = child_process.spawn('cmd', ['/c', cmds[0], op1]);
+
+	var dataOut = '';
+
+	function pumpData(data) {
+		if (data.indexOf('\n') < 0)
+			dataOut += data;
+		else {
+			broadcast.emit('serverMessage', dataOut);
+			dataOut = '';
+			broadcast.emit('serverMessage', data);
+		}
+	}
+
+	child.stdout.setEncoding('utf8');
+
+	child.stdout.on('data', function (data) {
+		//broadcast.emit('serverMessage', data);
+		pumpData(data);
+	});
+
+	/*
+	child_process.exec(cmd, {
+	encoding : 'utf8'
+	}, function (err, stdout, stderr) {
+	// the command exited or the launching failed
+	//console.log(stdout);
+	if (err) {
+	// we had an error launching the process
+	broadcast.emit('serverMessage', stderr);
+	} else {
+	var lines = stdout.split('\n');
+	//broadcast.emit('serverMessage', 'line# == ' + lines.length);
+	broadcast.emit('serverMessage', stdout);
+	return;
+	}
+	});
+	 */
+}
+
+io.sockets.on('connection', function (socket) {
+	console.log("connection " + (++count));
+
+	socket.on('clientMessage', function (content) {
+		socket.emit('serverMessage', 'You said: ' + content);
+		socket.get('username', function (err, username) {
+			if (!username) {
+				username = socket.id;
+			}
+			socket.get('room', function (err, room) {
+				if (err) {
+					throw err;
+				}
+				var broadcast = socket.broadcast;
+				var message = content;
+				socket.emit('serverMessage', 'You said: ' + content);
+				if (room) {
+					//broadcast.to(room);
+					socket.broadcast.to(room).emit('serverMessage', '[' + room + '] ' + username + ' said: ' + message);
+					run_cmd(broadcast, message);
+				} else {
+					//socket.emit('serverMessage', '[] No room to run command: ' + message);
+					broadcast.emit('serverMessage', '[] No room to run command: ' + message);
+				}
+			});
+		});
+
+	});
+	socket.on('login', function (username) {
+		socket.set('username', username, function (err) {
+			if (err) {
+				throw err;
+			}
+			socket.emit('serverMessage', 'Currently logged in as ' + username);
+			socket.broadcast.emit('serverMessage', 'User ' + username + ' logged in');
+		});
+	});
+	socket.on('disconnect', function () {
+		console.log("connection " + --count);
+
+		socket.get('username', function (err, username) {
+			if (!username) {
+				username = socket.id;
+			}
+			socket.broadcast.emit('serverMessage', 'User ' + username + ' disconnected');
+		});
+	});
+	socket.on('join', function (room) {
+		socket.get('room', function (err, oldRoom) {
+			if (err) {
+				throw err;
+			}
+			if (room != oldRoom) {
+				socket.set('room', room, function (err) {
+					if (err) {
+						throw err;
+					}
+					socket.get('username', function (err, username) {
+						if (!username) {
+							username = socket.id;
+						}
+						if (oldRoom) {
+							socket.broadcast.to(oldRoom).emit('serverMessage', '[' + oldRoom + '] User ' + username + ' left this room');
+							//console.log("Left room " + oldRoom);
+							socket.leave(oldRoom);
+						}
+						socket.join(room);
+						socket.emit('serverMessage', 'You joined room ' + room);
+						socket.broadcast.to(room).emit('serverMessage', '[' + room + '] User ' + username + ' joined this room');
+					});
+				});
+			} else {
+				socket.emit('serverMessage', 'You already joined room ' + room);
+				//console.log("Already joined " + room + " before!");
+			}
+		});
+	});
+	socket.on('leave', function (room) {
+		socket.get('room', function (err, oldRoom) {
+			if (err) {
+				throw err;
+			}
+			if (oldRoom && (oldRoom == room || room == "*")) {
+				//if (oldRoom && (oldRoom == room || room.trim() == "*")) {
+				socket.get('username', function (err, username) {
+					if (!username) {
+						username = socket.id;
+					}
+					socket.emit('serverMessage', 'You are leaving room ' + oldRoom);
+					socket.broadcast.to(oldRoom).emit('serverMessage', 'User ' + username + ' left this room');
+					socket.set('room', '', function (err) { //	clear association
+						if (err) {
+							throw err;
+						}
+						socket.leave(oldRoom);
+					});
+				});
+			} else {
+				socket.emit('serverMessage', "You haven't joined room " + room);
+			}
+		});
+	});
+	socket.emit('login', ++sno);
+});
